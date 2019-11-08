0b249a49e49a9c02030b7d83613412316f02628d
hungphan <colorvisavn@gmail.com>
2019-11-08 22:58:20 +0800

    PersistAsync Demo

---
+++ src/main/scala/chapter2/PersistAsyncDemo.scala
@@ -0,0 +1,63 @@
+package chapter2
+
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+import akka.persistence.PersistentActor
+import com.typesafe.config.ConfigFactory
+
+object PersistAsyncDemo extends App {
+
+  case class Command(contents: String)
+
+  case class Event(contents: String)
+
+  class CriticalStreamProcessor(eventAggregator: ActorRef) extends PersistentActor with ActorLogging {
+    override def receiveRecover: Receive = {
+      case message =>
+        log.info(s"Recovered: $message")
+    }
+
+    override def receiveCommand: Receive = {
+      case Command(contents) =>
+        eventAggregator ! s"Processing $contents"
+
+        persistAsync(Event(contents)) { e =>
+          eventAggregator ! e
+        }
+
+        // some actual computation
+      val processedContents = contents + "_processed"
+
+        persistAsync(Event(processedContents)) { e =>
+          eventAggregator ! e
+        }
+    }
+
+    override def persistenceId: String = "critical-stream-processor"
+
+  }
+
+  object CriticalStreamProcessor {
+    def props(eventAggregator: ActorRef) = Props(new CriticalStreamProcessor(eventAggregator))
+  }
+
+  class EventAggregator extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message => log.info(message.toString)
+    }
+  }
+
+  val system = ActorSystem("PersistAsyncDemo", ConfigFactory.load().getConfig("chapter2"))
+  val eventAggregator = system.actorOf(Props[EventAggregator], "eventAggregator")
+  val streamProcessor = system.actorOf(CriticalStreamProcessor.props(eventAggregator), "streamProcessor")
+
+  streamProcessor ! Command("command1")
+  streamProcessor ! Command("command2")
+
+  /**
+   * persistAsync vs persist
+   * - perf: high-throughput environments
+   *
+   * persist vs persisAsync
+   * - ordering guarantees
+   */
+}
