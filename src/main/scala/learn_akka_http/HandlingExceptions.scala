e7d4cb65ae3d745b813e711a7dcd2a5031344cce
hungphan <colorvisavn@gmail.com>
2019-12-27 17:27:56 +0800

    Handle exceptions for akka HTTP

---
+++ src/main/scala/learn_akka_http/HandlingExceptions.scala
@@ -0,0 +1,75 @@
+package learn_akka_http
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.StatusCodes
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.ExceptionHandler
+
+object HandlingExceptions extends App {
+  implicit val system = ActorSystem("HandlingExceptions")
+  implicit val materializer = ActorMaterializer()
+  import system.dispatcher
+
+  val simpleRoute =
+    path("api" / "people") {
+      get {
+        // directive that throws some exceptions
+        throw new RuntimeException("Getting all the people took too long")
+      } ~
+        post {
+          parameter(Symbol("id")) { id =>
+            if (id.length > 2) {
+              throw new NoSuchElementException(
+                s"Parameter $id cannot be found in the database, TABLE FLIP!"
+              )
+            }
+
+            complete(StatusCodes.OK)
+          }
+        }
+    }
+
+  implicit val customExceptionHandler: ExceptionHandler = ExceptionHandler {
+    case g: RuntimeException =>
+      complete(StatusCodes.NotFound, g.getMessage)
+    case g: IllegalArgumentException =>
+      complete(StatusCodes.BadRequest, g.getMessage)
+  }
+
+//  Http().bindAndHandle(simpleRoute, "localhost", 8080)
+
+  val runtimeExceptionHandler: ExceptionHandler = ExceptionHandler {
+    case g: RuntimeException =>
+      complete(StatusCodes.NotFound, g.getMessage)
+  }
+
+  val noSuchElementExceptionHandler: ExceptionHandler = ExceptionHandler {
+    case g: NoSuchElementException =>
+      complete(StatusCodes.BadRequest, g.getMessage)
+  }
+
+  val delicateHandleRoute =
+    handleExceptions(runtimeExceptionHandler) {
+      path("api" / "people") {
+        get {
+          // directive that throws some exceptions
+          throw new RuntimeException("Getting all the people took too long")
+        } ~
+          handleExceptions(noSuchElementExceptionHandler) {
+            post {
+              parameter(Symbol("id")) { id =>
+                if (id.length > 2) {
+                  throw new NoSuchElementException(
+                    s"Parameter $id cannot be found in the database, TABLE FLIP!"
+                  )
+                }
+
+                complete(StatusCodes.OK)
+              }
+            }
+          }
+      }
+    }
+}
