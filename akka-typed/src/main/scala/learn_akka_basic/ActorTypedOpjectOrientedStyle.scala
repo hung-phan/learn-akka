7b65d79221a934f3a52a479c27ccdb592b0e39a1
hungphan <colorvisavn@gmail.com>
2020-01-11 17:58:24 +0800

    Add object-oriented style for Akka actor

---
+++ akka-typed/src/main/scala/learn_akka_basic/ActorTypedOpjectOrientedStyle.scala
@@ -0,0 +1,24 @@
+package learn_akka_basic
+
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
+
+object ActorTypedOpjectOrientedStyle {
+  class WordCounterActor(context: ActorContext[String])
+      extends AbstractBehavior[String](context) {
+    var total = 0
+
+    override def onMessage(msg: String): Behavior[String] = {
+      context.log.info(s"Receive: $msg")
+
+      msg match {
+        case "STOP" =>
+          Behaviors.stopped
+        case _ =>
+          total += msg.split(" ").length
+          context.log.info(s"New count: $total")
+          this
+      }
+    }
+  }
+}
